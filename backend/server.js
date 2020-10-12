c68b01570a4b68dd64ec3225e7a9e3c4833f3c1c
changming.h <changming.h@digipen.edu>
2020-10-12 22:36:29 +0800

    publish

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,11 @@ app.get('/api/get',(req,res)=>{
         res.send(result);
     })
 })
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
 app.post('/api/insert',(req,res)=>{ 
     const movieName = req.body.movieName;
     const movieReview = req.body.movieReview;
